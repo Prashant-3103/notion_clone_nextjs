4b997b5d0342d9b63f1eb24dc31eeeb21903562f
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-12 16:55:39 +0530

    landing page finished

--- app/(marketing)/_components/Heros.tsx
+++ app/(marketing)/_components/Heros.tsx
@@ -5,11 +5,13 @@ const Heros = () => {
     <div className="flex flex-col items-center justify-center max-w-5xl">
         <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
-<Image fill alt="documents" src="/documents.png" className="object-contain"/>
+<Image fill alt="documents" src="/documents.png" className="object-contain dark:hidden"/>
+<Image fill alt="documents" src="/documents-dark.png" className="object-contain hidden dark:block"/>
 
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
-<Image src="/reading.png" alt="raeding" fill className="object-contain"/>
+<Image src="/reading.png" alt="raeding" fill className="object-contain dark:hidden"/>
+<Image src="/reading-dark.png" alt="raeding" fill className="object-contain dark:block hidden"/>
 </div>
     </div>
         </div>
