8ebe0d658f44dca874a0460dac2fd8dc4abbe414
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-12 16:37:48 +0530

    setting up darkmode shadcn-ui

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useScrollTop } from '@/hooks/use-scroll-top'
 import { cn } from '@/lib/utils'
 import React from 'react'
 import { Logo } from './Logo'
+import { ModeToggle } from '@/components/mode-toggle'
 
 const Navbar = () => {
     const scrolled = useScrollTop()
@@ -10,7 +11,7 @@ const Navbar = () => {
     <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
 <Logo/>
 <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
-login
+<ModeToggle/>
 </div>
     </div>
   )
