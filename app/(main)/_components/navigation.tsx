642d7ca809aea4dfade011e98a34a5b0f528b321
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-20 00:44:03 +0530

    error solved navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -45,14 +45,30 @@ export const Navigation = () => {
   const navbarRef = useRef<ElementRef<"div">>(null);
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
 
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      navbarRef.current.style.setProperty(
+        "left",
+        isMobile ? "100%" : "240px"
+      );
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
   useEffect(() => {
     if (isMobile) {
       collapse();
     } else {
-      resetWidth();
+     resetWidth()
     }
-  }, [isMobile]);
+  }, [isMobile,resetWidth]);
 
   useEffect(() => {
     if (isMobile) {
@@ -91,23 +107,7 @@ export const Navigation = () => {
     document.removeEventListener("mouseup", handleMouseUp);
   };
 
-  const resetWidth = () => {
-    if (sidebarRef.current && navbarRef.current) {
-      setIsCollapsed(false);
-      setIsResetting(true);
 
-      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-      navbarRef.current.style.setProperty(
-        "width",
-        isMobile ? "0" : "calc(100% - 240px)"
-      );
-      navbarRef.current.style.setProperty(
-        "left",
-        isMobile ? "100%" : "240px"
-      );
-      setTimeout(() => setIsResetting(false), 300);
-    }
-  };
 
   const collapse = () => {
     if (sidebarRef.current && navbarRef.current) {
