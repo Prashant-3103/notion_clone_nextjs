3f1a4c7cb6cf4a1a6f792a4dd398be125be322af
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-20 00:09:44 +0530

    publishing the note

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PublicLayout = ({
+    children
+}: {children: React.ReactNode}) => {
+    return (
+        <div className="h-4 dark:bg-[#1f1f1f]">
+{children}
+        </div>
+      );
+}
+
+export default PublicLayout;
+{
+
+}
