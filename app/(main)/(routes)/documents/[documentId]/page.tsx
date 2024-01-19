9ef0eb3a81e43d6542a9096e585c7ea071a7c06a
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-19 13:31:02 +0530

    updating title in the documentId page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -2,15 +2,30 @@
 import { useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
+import { ToolBar } from "@/components/toolbar"
 interface DocumentIdPageProps {
     params: {
         documentId: Id<"documents">
     }
 }
 const DocumentIdPage = ({params}: DocumentIdPageProps)=>{
+    const document = useQuery(api.documents.getById,{
+        documentId: params.documentId
+    })
+    if(document===undefined){
+     return  ( <div>
+            loading....
+        </div>)
+    }
+if(document === null){
+    return <div>Not found</div>
+}
     return(
-        <div>
-        document id
+        <div className="pb-40">
+            <div className="h-[35vh]"/>
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+<ToolBar initialData={document}/>
+        </div>
         </div>
     )
 }
