c9c3276b116f814cf1a85810463aaa26e8b9dda4
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2024-01-20 13:15:56 +0530

    confirm model error fixed

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -1,4 +1,4 @@
-import e from "express"
+
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog"
 import React from "react"
 
